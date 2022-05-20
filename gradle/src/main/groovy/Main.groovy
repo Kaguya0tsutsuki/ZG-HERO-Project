4db96fb8f467e3c2b25ebb74c1f321bdbcddabe4
Kaguya <jpedrorteles@hotmail.com>
2022-05-19 22:58:58 -0300

    gradle

---
+++ gradle/src/main/groovy/Main.groovy
@@ -0,0 +1,212 @@
+class Main {
+
+
+    static void main(String[] args) {
+        menuLogin()
+
+    }
+
+    static menuOpcaoLogin(def opcao){
+        switch (opcao){
+            case "1":
+                menuCandidato()
+                break
+            case "2":
+                menuEmpresa()
+                break
+            case "3":
+                menuVaga()
+                break
+            case "4":
+                menuCompetencia()
+                break
+            case "5":
+                System.exit(0)
+                break
+        }
+    }
+
+    static menuOpcaoCandidato(def opcao){
+        Candidato candidato = new Candidato()
+
+        switch (opcao){
+            case "1":
+                candidato.criarCandidato()
+                menuCandidato()
+                break
+
+            case "2":
+                candidato.printCandidato()
+                menuCandidato()
+                break
+
+            case "3":
+                candidato.excluirCandidato()
+                menuCandidato()
+                break
+
+            case "4":
+                menuLogin()
+                break
+
+            case "5":
+                System.exit(0)
+                break
+        }
+    }
+
+    static menuOpcaoEmpresa(def opcao){
+        Empresa empresa = new Empresa()
+
+        switch (opcao){
+            case "1":
+                empresa.criarEmpresa()
+                menuEmpresa()
+                break
+
+            case "2":
+                empresa.printEmpresa()
+                menuEmpresa()
+                break
+
+            case "3":
+                empresa.excluirEmpresa()
+                menuEmpresa()
+                break
+
+            case "4":
+                menuLogin()
+                break
+
+            case "5":
+                System.exit(0)
+                break
+        }
+
+    }
+
+    static menuOpcaoVaga(def opcao){
+        Vaga vaga = new Vaga()
+
+        switch (opcao){
+            case "1":
+                vaga.criarVaga()
+                menuVaga()
+                break
+
+            case "2":
+                vaga.printVaga()
+                menuVaga()
+                break
+
+            case "3":
+                vaga.excluirVaga()
+                menuVaga()
+                break
+
+            case "4":
+                menuLogin()
+                break
+
+            case "5":
+                System.exit(0)
+                break
+        }
+    }
+
+    static menuOpcaoCompetencia(def opcao){
+        Competencia competencia = new Competencia()
+
+        switch (opcao){
+            case "1":
+                competencia.criarCompetencia()
+                menuCompetencia()
+                break
+            case "2":
+                competencia.printCompetencia()
+                menuCompetencia()
+                break
+            case "3":
+                competencia.excluirCompetencia()
+                menuCompetencia()
+                break
+            case "4":
+                menuLogin()
+                break
+            case "5":
+                System.exit(0)
+                break
+        }
+    }
+
+    static menuLogin(){
+
+        print "\nDigite :\n" + "1 - Candidato.\n" + "2 - Empresa.\n" + "3 - Vaga.\n" + "4 - Competencia.\n" + "5 - Sair.\n"
+        def opcao = System.in.newReader().readLine()
+        if (opcao == '0' || opcao > '5'){
+            print "Opção Inválida!"
+            for(opcao = System.in.newReader().readLine(); opcao == '0' || opcao > '5'; opcao = System.in.newReader().readLine()) {
+                print "Opção Inválida!"
+            }
+        }
+
+        menuOpcaoLogin(opcao)
+    }
+
+    static menuCandidato(){
+
+        print "\nDigite :\n" + "1 - Criar Candidato.\n" + "2 - Imprimir Candidato.\n" + "3 - Excluir Candidato.\n" + "4 - Voltar.\n" + "5 - Sair.\n"
+        def opcao = System.in.newReader().readLine()
+        if (opcao == '0' || opcao > '5'){
+            print "Opção Inválida!"
+            for(opcao = System.in.newReader().readLine(); opcao == '0' || opcao > '5'; opcao = System.in.newReader().readLine()) {
+                print "Opção Inválida!"
+            }
+        }
+
+        menuOpcaoCandidato(opcao)
+    }
+
+    static menuEmpresa(){
+
+        print "\nDigite :\n" + "1 - Criar Empresa.\n" + "2 - Imprimir Empresa.\n" + "3 - Excluir Empresa.\n" + "4 - Voltar.\n" + "5 - Sair.\n"
+        def opcao = System.in.newReader().readLine()
+        if (opcao == '0' || opcao > '5'){
+            print "Opção Inválida!"
+            for(opcao = System.in.newReader().readLine(); opcao == '0' || opcao > '5'; opcao = System.in.newReader().readLine()) {
+                print "Opção Inválida!"
+            }
+        }
+
+        menuOpcaoEmpresa(opcao)
+    }
+
+    static menuVaga(){
+
+        print "\nDigite :\n" + "1 - Criar Vaga.\n" + "2 - Imprimir Vaga.\n" + "3 - Excluir Vaga.\n" + "4 - Voltar.\n" + "5 - Sair.\n"
+        def opcao = System.in.newReader().readLine()
+        if (opcao == '0' || opcao > '5'){
+            print "Opção Inválida!"
+            for(opcao = System.in.newReader().readLine(); opcao == '0' || opcao > '5'; opcao = System.in.newReader().readLine()) {
+                print "Opção Inválida!"
+            }
+        }
+
+        menuOpcaoVaga(opcao)
+    }
+
+    static menuCompetencia(){
+
+        print "\nDigite :\n" + "1 - Criar Competencia.\n" + "2 - Imprimir Competencia.\n" + "3 - Excluir Competencia.\n" + "4 - Voltar.\n" + "5 - Sair.\n"
+        def opcao = System.in.newReader().readLine()
+        if (opcao == '0' || opcao > '5'){
+            print "Opção Inválida!"
+            for(opcao = System.in.newReader().readLine(); opcao == '0' || opcao > '5'; opcao = System.in.newReader().readLine()) {
+                print "Opção Inválida!"
+            }
+        }
+
+        menuOpcaoCompetencia(opcao)
+    }
+
+}
---
+++ gradle/src/main/groovy/Vaga.groovy
@@ -0,0 +1,94 @@
+class Vaga {
+
+    public criarVaga(){
+        BD bd = new BD()
+
+        print "Nome: "
+        def nomeVaga = System.in.newReader().readLine()
+
+        if(nomeVaga == '') {
+            print("\nO nome digitado não atende aos requisitos!")
+            print("\nDigite um nome valido!")
+
+            for (nomeVaga = System.in.newReader().readLine(); nomeVaga == ''; nomeVaga = System.in.newReader().readLine()) {
+                print("\nO nome digitado não atende aos requisitos!")
+                print("\nDigite um nome valido!")
+            }
+        }
+
+        print "Descrição: "
+        def descricaoVaga = System.in.newReader().readLine()
+
+        if(descricaoVaga == '' || descricaoVaga.length() == 100) {
+            print("\nA descrição digitada não atende aos requisitos!")
+            print("\nDigite uma descrição valida!")
+
+            for (descricaoVaga = System.in.newReader().readLine(); descricaoVaga == '' || descricaoVaga.length() == 100; descricaoVaga = System.in.newReader().readLine()) {
+                print("\nA descrição digitada não atende aos requisitos!")
+                print("\nDigite uma descrição valida!")
+            }
+        }
+
+        print "Local: "
+        def localVaga = System.in.newReader().readLine()
+
+        if(localVaga == '') {
+            print("\nO local digitado não atende aos requisitos!")
+            print("\nDigite um local valido!")
+
+            for (localVaga = System.in.newReader().readLine(); localVaga == ''; localVaga = System.in.newReader().readLine()) {
+                print("\nO local digitado não atende aos requisitos!")
+                print("\nDigite um local valido!")
+            }
+        }
+
+        bd.printEmpresaBD()
+
+        print "Digite o ID da empresa que atende a vaga: "
+        def empresaVaga = System.in.newReader().readLine()
+
+        if(bd.connectedList("SELECT * FROM empresa WHERE id = "+empresaVaga+";") == '' || empresaVaga == '') {
+            print("\nO id digitado não atende aos requisitos!")
+            print("\nDigite um id valido!")
+
+            for (empresaVaga = System.in.newReader().readLine(); bd.connectedList("SELECT * FROM empresa WHERE id = "+empresaVaga+";") == '' || empresaVaga == ''; empresaVaga = System.in.newReader().readLine()) {
+                print("\nO id digitado não atende aos requisitos!")
+                print("\nDigite um id valido!")
+            }
+        }
+
+        bd.criarVagaBD(nomeVaga, descricaoVaga, localVaga, empresaVaga)
+
+        print "Vaga criada com sucesso!"
+    }
+
+    public printVaga(){
+        BD bd = new BD()
+
+        bd.printVagaBD()
+    }
+
+    public excluirVaga(){
+        BD bd = new BD()
+
+        bd.printVagaBD()
+
+        print "ID: "
+        def idVaga = System.in.newReader().readLine()
+
+        if(bd.connectedList("SELECT * FROM vaga WHERE id = "+idVaga) == '' || idVaga == '') {
+            print("\nO id digitado não atende aos requisitos!")
+            print("\nDigite um id valido!")
+
+            for (idVaga = System.in.newReader().readLine(); bd.connectedList("SELECT * FROM vaga WHERE id = "+idVaga) == '' || idVaga == ''; idVaga = System.in.newReader().readLine()) {
+                print("\nO id digitado não atende aos requisitos!")
+                print("\nDigite um id valido!")
+            }
+        }
+
+        bd.excluirVagaBD(idVaga)
+        bd.printVagaBD()
+
+        print "Vaga excluida com sucesso!"
+    }
+}
